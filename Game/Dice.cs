cb25b2672ae3bd6d6fe49a8d2125f2638ea3b718
huchifun <36794833+huchifun@users.noreply.github.com>
2020-06-30 01:03:29 +0600

    initial commit

---
+++ Game/Dice.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class Dice
+    {
+        public int Number { get; private set; }
+
+        public void Roll()
+        {
+            Number = Utils.GetRandom(1, 6);
+            Print(Number);
+        }
+
+        public void Roll(int number)
+        {
+            Number = number;
+            Print(Number);
+        }
+
+        public static int operator +(Dice d1, Dice d2)
+        {
+            return d1.Number + d2.Number;
+        }
+
+        public static bool operator >(Dice d1, Dice d2)
+        {
+            return d1.Number > d2.Number;
+        }
+
+        public static bool operator <(Dice d1, Dice d2)
+        {
+            return d1.Number < d2.Number;
+        }
+
+        void Print(int number)
+        {
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("---------\n|       |\n|   #   |\n|       |\n---------");
+                    break;
+                case 2:
+                    Console.WriteLine("---------\n| #     |\n|       |\n|     # |\n---------");
+                    break;
+                case 3:
+                    Console.WriteLine("---------\n| #     |\n|   #   |\n|     # |\n---------");
+                    break;
+                case 4:
+                    Console.WriteLine("---------\n| #   # |\n|       |\n| #   # |\n---------");
+                    break;
+                case 5:
+                    Console.WriteLine("---------\n| #   # |\n|   #   |\n| #   # |\n---------");
+                    break;
+                case 6:
+                    Console.WriteLine("---------\n| #   # |\n| #   # |\n| #   # |\n---------");
+                    break;
+            }
+        }
+    }
+}
---
+++ Game/Utils.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class Utils
+    {
+        private static Random _random;
+
+        public static void Currentscore(int userscore, int compscore)
+        {
+            Print("---------- Current score ---------");
+            Print($"User:\t\t{userscore} points\nComputer:\t{compscore} points\n");
+        }
+
+        public static void PrintResults(Round[] rounds)
+        {
+            Print("\n #\t|\t\t| Comp\t|User\t");
+            Print("-------------------------");
+
+            int userTotal = 0;
+            int compTotal = 0;
+
+            foreach (Round round in rounds)
+            {
+                Print($" \t| Predicted:\t| {round.CompResult.Predicted}\t|{round.UserResult.Predicted}\t");
+                Print($" {round.Id}\t| Dice:\t\t| {round.CompResult.DiceResult}\t|{round.UserResult.DiceResult}\t");
+                Print($" \t| Penalty:\t| {round.CompResult.Penalty}\t|{round.UserResult.Penalty}\t");
+                Print($" \t| Score:\t| {round.CompResult.Score}\t|{round.UserResult.Score}\t");
+                Print("-------------------------");
+
+                userTotal += round.UserResult.Score;
+                compTotal += round.CompResult.Score;
+
+            }
+
+            Print($" \t| Total Score:\t| {compTotal}\t|{userTotal}\t");
+            Print("-------------------------\n");
+
+            int dif = Math.Abs(userTotal - compTotal);
+
+            if (userTotal > compTotal)
+            {
+                Print($"User wins by {dif}");
+            }
+            else if (userTotal < compTotal)
+            {
+                Print($"Comp wins by {dif}");
+            }
+            else
+            {
+                Print("Ничья!");
+            }
+        }
+
+        public static void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void Print(int message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void Print(string message, string v)
+        {
+            Console.WriteLine(message, v);
+        }
+
+        public static int GetInt(string message)
+        {
+            Print(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static int GetRandom(int from, int to)
+        {
+            Random rnd = CreateRandomizer();
+
+            return rnd.Next(from, to + 1);
+        }
+
+
+        // синглтон
+        private static Random CreateRandomizer()
+        {
+            if (_random == null)
+            {
+                _random = new Random();
+            }
+
+            return _random;
+        }
+    }
+}
